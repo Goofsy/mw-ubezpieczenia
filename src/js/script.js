375a37a0c7f8058d3017b43d312205228b173667
goofsy <dawid.stud@gmail.com>
2021-02-02 04:06:40 +0100

    added form and info in contact section

--- src/js/script.js
+++ src/js/script.js
@@ -89,6 +89,139 @@ class Navbar {
 }
 new Navbar();
 
+class SendEmail {
+  _form = document.querySelector('.form');
+  _formInfo = document.querySelector('.form__info');
+  _formSpinner = document.querySelector('.form__spinner');
+  _inputName = document.querySelector('#name');
+  _inputEmail = document.querySelector('#email');
+  _inputPhone = document.querySelector('#phone');
+  _inputMessage = document.querySelector('#message');
+
+  constructor() {
+    this._handlerForm();
+    this._addFilledClass();
+    this._removeErrorClassHandler();
+  }
+
+  _handlerForm() {
+    this._form.addEventListener('submit', this._handleForm.bind(this));
+  }
+
+  async _handleForm(e) {
+    e.preventDefault();
+    try {
+      const _inputNameValue = this._inputName.value;
+      const _inputEmailValue = this._inputEmail.value;
+      const _inputPhoneValue = this._inputPhone.value;
+      const _inputMessageValue = this._inputMessage.value;
+
+      if (
+        _inputNameValue.trim().length < 1 ||
+        _inputNameValue === null ||
+        _inputNameValue === ''
+      ) {
+        this._inputName.classList.add('form__group__input--error');
+        throw new Error('Pole nie może byc puste!');
+      }
+
+      if (!this._validateEmailAddress(_inputEmailValue)) {
+        this._inputEmail.classList.add('form__group__input--error');
+        throw new Error('Niepoprawny adres e-mail!');
+      }
+
+      if (
+        _inputPhoneValue.trim().length < 8 ||
+        _inputPhoneValue === null ||
+        _inputPhoneValue === ''
+      ) {
+        this._inputPhone.classList.add('form__group__input--error');
+        throw new Error('Nr telefonu musi zawierać przynajmniej 8 znaków!');
+      }
+
+      if (
+        _inputMessageValue.trim().length < 1 ||
+        _inputMessageValue === null ||
+        _inputMessageValue === ''
+      ) {
+        this._inputMessage.classList.add('form__group__input--error');
+        throw new Error('Pole nie może byc puste!');
+      }
+
+      this._formSpinner.style.display = 'inline-block';
+
+      const send = await this._sendEmail(
+        _inputNameValue,
+        _inputEmailValue,
+        _inputPhoneValue,
+        _inputMessageValue
+      );
+      this._formSpinner.style.display = 'none';
+
+      if (send !== 'OK') throw new Error('Błąd połączenia');
+      this._resetForm();
+    } catch (err) {
+      this._formInfo.innerHTML = err.message;
+      this._formInfo.classList.add('form__info--error');
+    }
+  }
+
+  _removeErrorClassHandler() {
+    this._form.addEventListener('click', this._removeErrorClass.bind(this));
+  }
+
+  _removeErrorClass(e) {
+    if (!e.target.closest('.form__group__input--error')) return;
+    e.target.classList.remove('form__group__input--error');
+    this._formInfo.innerHTML = '';
+    this._formInfo.classList.remove('form__info--error');
+  }
+
+  _addFilledClass() {
+    this._form.addEventListener('change', e => {
+      if (e.target.value.length === 0 || e.target.value === '')
+        e.target.classList.remove('form__group__input--filled');
+      else {
+        e.target.classList.add('form__group__input--filled');
+      }
+    });
+  }
+  _validateEmailAddress(email) {
+    const re = /\S+@\S+\.\S+/;
+    return re.test(email);
+  }
+
+  _sendEmail(name, email, phone, message) {
+    try {
+      return Email.send({
+        SecureToken: '1b17ee09-4190-4d7c-bd83-efeb38674305',
+        To: 'dawid.stud@gmail.com',
+        From: email,
+        Subject: 'E-mail ze stronki',
+        Body: `${name}, tel: ${phone}, Wiadomość: ${message}`,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  _resetForm() {
+    this._form.reset();
+    const [...formInputs] = document.querySelectorAll('.form__group__input');
+
+    formInputs.forEach(input => {
+      input.classList.remove('form__group__input--filled');
+    });
+
+    this._formInfo.innerHTML = 'Wiadomośc została wysłana';
+
+    setTimeout(() => {
+      this._formInfo.innerHTML = '';
+    }, 4000);
+  }
+}
+new SendEmail();
+
 class InfiniteSlider {
   constructor(
     animTime = '10000',
