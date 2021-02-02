42882e72f85a1dae9ab3e6f2e6888d2146c8629a
goofsy <dawid.stud@gmail.com>
2021-02-02 23:42:41 +0100

    code optimalization v1

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,4 @@
 import 'core-js';
-import 'regenerator-runtime/runtime';
 import $ from 'jquery';
 import { Loader } from '@googlemaps/js-api-loader';
 
@@ -231,7 +230,7 @@ class SendEmail {
         Body: `${name}, tel: ${phone}, Wiadomość: ${message}`,
       });
     } catch (err) {
-      console.log(err);
+      alert(err);
     }
   }
 
@@ -264,14 +263,13 @@ class InfiniteSlider {
     this.oldWidth = 0;
     this.duration = parseInt(animTime);
     this.start = 0;
-    this.refresh = 0; //0, 1, or 2, as in steps of the animation
+    this.refresh = 0;
     this._prevStop = false;
     this._stop = false;
     this._oldTimestamp = 0;
   }
 
   animate() {
-    /* fix for browsers who like to run JS before images are loaded */
     const imgs = Array.prototype.slice
       .call(this.slider.querySelectorAll('img'))
       .filter(img => {
@@ -282,17 +280,14 @@ class InfiniteSlider {
       return;
     }
 
-    /* Add another copy of the slideshow to the end, keep track of original width */
     this.oldWidth = this.slider.offsetWidth;
     const sliderText =
       '<span class="slider-extra">' + this.slider.innerHTML + '</span>';
     this.slider.innerHTML += sliderText;
 
-    /* can have content still when we move past original slider */
     this.width = this.slider.offsetWidth;
     const minWidth = 2 * screen.width;
 
-    /* Add more slideshows if needed to keep a continuous stream of content */
     while (this.width < minWidth) {
       this.slider.innerHTML += sliderText;
       this.width = this.slider.width;
@@ -301,7 +296,6 @@ class InfiniteSlider {
       .querySelector('.slider-extra:last-child')
       .classList.add('slider-last');
 
-    /* loop animation endlesssly (this is pretty cool) */
     window.requestAnimationFrame(this.controlAnimation.bind(this));
   }
 
@@ -320,7 +314,6 @@ class InfiniteSlider {
   }
 
   controlAnimation(timestamp) {
-    //console.log('this.stop: ' + this._stop + '\nthis.prevStop: ' + this._prevStop)
     if (this._stop === true) {
       if (this._prevStop === false) {
         this.slider.style.marginLeft = getComputedStyle(this.slider).marginLeft;
@@ -331,7 +324,6 @@ class InfiniteSlider {
       this._prevStop = false;
       this.start = this.start + (timestamp - this._oldTimestamp);
     } else {
-      //reset animation
       if (this.refresh >= 1) {
         this.start = timestamp;
         this.slider.style.marginLeft = 0;
@@ -364,24 +356,20 @@ function detectIE() {
   var msie = ua.indexOf('MSIE ');
 
   if (msie > 0) {
-    // IE 10 or older => return version number
     return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
   }
 
   var trident = ua.indexOf('Trident/');
   if (trident > 0) {
-    // IE 11 => return version number
     var rv = ua.indexOf('rv:');
     return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
   }
 
   var edge = ua.indexOf('Edge/');
   if (edge > 0) {
-    // Edge (IE 12+) => return version number
     return parseInt(ua.substring(edge + 5, ua.indexOf('.', edge)), 10);
   }
 
-  // other browser
   return false;
 }
 
@@ -389,12 +377,10 @@ document.addEventListener('DOMContentLoaded', function () {
   const slider = new InfiniteSlider(20000);
   const ie = detectIE();
 
-  //Dont animate under IE10, just place the images
   if (ie !== false && ie < 10) {
     slider.stagnate();
     return;
   }
-  //IE 11 and lower, fix for width *increasing* as more of the slider is shown
   if (ie !== false && ie < 12) {
     slider.getIeWidth();
   }
